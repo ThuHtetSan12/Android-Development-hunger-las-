1964e2b79486ddf47bdb987cf4e404fb9ecc33ad
thu-htetsan <THUHTET12.LMF@gmail.com>
2023-11-15 13:45:42 +0800

    create project

---
+++ app/src/main/java/com/example/hunger_las_dit2b02/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hunger_las_dit2b02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
