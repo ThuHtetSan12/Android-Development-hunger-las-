de269239c367b98d7994b7edeab8501a3cad66d3
thu-htetsan <THUHTET12.LMF@gmail.com>
2024-01-29 01:35:44 +0800

    authen from backup

--- app/src/main/java/com/example/hunger_las_dit2b02/MainActivity.java
+++ app/src/main/java/com/example/hunger_las_dit2b02/MainActivity.java
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.hunger_las_dit2b02.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (!isUserLoggedIn()) {
+            // If not logged in, go to the login page
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+            finish(); // Finish the current activity to prevent going back to it
+        } else {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         replaceFragment(new HomeFragment());
@@ -40,10 +49,17 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
     }
+}
     private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
+    }
+
+    private boolean isUserLoggedIn() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        return user != null; // Returns true if a user is currently authenticated, false otherwise
     }
 }
